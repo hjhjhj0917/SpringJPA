4e9cf1252cabd5194c1a2e06fa09a7a8a657465d
Yang jun mo <abc22842199@gmail.com>
2026-03-18 15:21:31 +0900

    융합프로젝트실습 JPA실습

--- src/main/java/kopo/poly/repository/NoticeFetchRepository.java
+++ src/main/java/kopo/poly/repository/NoticeFetchRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface NoticeFetchRepository extends JpaRepository<NoticeFetchEntity, Long> {
+public interface NoticeFetchRepository extends JpaRepository<NoticeJoinEntity, Long> {
 
     @Query("SELECT A FROM NoticeFetchEntity A JOIN FETCH A.userInfo ORDER BY A.noticeYn desc, A.noticeSeq DESC")
     List<NoticeFetchEntity> getListFetchJoin();
